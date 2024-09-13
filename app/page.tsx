ff09cd1cf59de2d1fa777c9c0cc01c6bfd7cf2e3
Stephan Kulla <git.mail@kulla.me>
2024-09-13 14:38:02 +0200

    feat: Add serlo editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 'use client'
 
-import { Heading } from '@radix-ui/themes'
+import { SerloEditor, SerloEditorProps, SerloRenderer } from '@serlo/editor'
+import { Card, Heading } from '@radix-ui/themes'
+import React from 'react'
 
 export default function Home() {
+  const [state, setState] = React.useState<SerloEditorProps['initialState']>({
+    plugin: 'rows',
+  })
+
   return (
     <main>
-      <Heading>Hello World</Heading>
+      <Heading>Test Serlo editor</Heading>
+      <Card>
+        <SerloEditor
+          initialState={state}
+          editorVariant="unknown"
+          onChange={setState}
+        >
+          {(editor) => <div>{editor.element}</div>}
+        </SerloEditor>
+      </Card>
+      <Heading>State</Heading>
+      <Card>
+        <pre>{JSON.stringify(state, null, 2)}</pre>
+      </Card>
+      <Heading>Renderer</Heading>
+      <Card>
+        <SerloRenderer state={state} editorVariant="unknown" />
+      </Card>
     </main>
   )
 }
